ec108f218b177a3ff18f9290a6609ac4cd96e1d9
ksleima <ksleima@us.ibm.com>
2017-05-19 07:14:59 -0400

    removed comments

--- src/wasdev/sample/servlet/WestfieldServiceServlet.java
+++ src/wasdev/sample/servlet/WestfieldServiceServlet.java
@@ -27,8 +27,6 @@ public abstract class WestfieldServiceServlet  extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 
-	//private static final String WESTFIELD_HOST = "https://servicestest.westfieldgrp.com:44330";
-
 	private String soapRequestUrl;
 	
 	@Override
